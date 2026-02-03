c360fba5d2b8b7bd36523076e500bf55f0274332
J-Nguyen-web <map.genshin.impacts@gmail.com>
2026-02-03 23:22:33 +0200

    add about and sights screens

--- screens/SightsScreen.jsx
+++ screens/SightsScreen.jsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { View, Text, StyleSheet } from "react-native";
 
-export default function HomeScreen() {
+export default function SightsScreen() {
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
