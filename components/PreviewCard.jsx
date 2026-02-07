2e6898f44746edf18a78e6778f2899b7a1273fa9
J-Nguyen-web <map.genshin.impacts@gmail.com>
2026-02-07 23:56:46 +0200

    Create PreviewCard.jsx

---
+++ components/PreviewCard.jsx
@@ -0,0 +1,19 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+
+export default function PreviewCard() {
+    const navigation = useNavigation();
+
+
+    return (
+        <Pressable onPress={ () => navigation.navigate('Gallery')}>
+            <View>
+                <Image />                
+            </View>
+            <View>
+                <Text>dynamic - Recent or Most Liked</Text>
+                {/* TODO Dynamic title */}
+            </View>
+        </Pressable>
+    );
+}
