4e497cca1da78f6ad12fa7544cdec61ec3d2e7b0
J-Nguyen-web <map.genshin.impacts@gmail.com>
2026-02-12 23:56:13 +0200

    create sights details screen

---
+++ src/screens/SightDetailsScreen.jsx
@@ -0,0 +1,32 @@
+import { Text } from "@react-navigation/elements";
+import { Button, Image } from "react-native";
+
+export default function SightDetailsScreen({
+    titleImage,
+    title,
+    rating,
+    location,
+    country,
+    description,
+
+}) {
+    return (
+    <View>    
+        <Image source={{ uri: titleImage }} style={cardStyles.image} />
+        <View style={cardStyles.content}>
+            <View style={cardStyles.titleRow}>
+                <Text style={cardStyles.title}>{title}</Text>
+                <Text style={cardStyles.rating}># {rating}</Text>
+            </View>
+    {/* todo add flags for country */}
+            <Text style={cardStyles.location}>{location} ({country})</Text>
+            <Text>{description}</Text>
+            <View>
+                <Button>Edit</Button>
+                <Button>Delete</Button>
+                <Button>Rate</Button>
+            </View>
+        </View>
+    </View>
+    );
+}
