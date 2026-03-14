36a79e2847fb4e75b56d49e4d192d28b6f42d7a6
J-Nguyen-web <map.genshin.impacts@gmail.com>
2026-03-14 23:29:54 +0200

    recreate Profile Navigator

--- src/navigators/ProfileNavigatror.jsx
+++ src/navigators/ProfileNavigatror.jsx
@@ -1,13 +1,32 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ProfileScreen from "../screens/ProfileScreen.jsx";
+import { globalColor } from "../globalStyles.js";
+import MySightsScreen from "../screens/MySightsScreen.jsx";
 
-export default function SigthsNavigator() {
+export default function ProfileNavigator() {
     const Stack = createNativeStackNavigator();
     return (
         
         // всички screens в който ще са със stack navigator (със стрелка за back, за да не се stack множество екрани)
         <Stack.Navigator>
-            <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen 
+                name="ProfileDetails" 
+                component={ProfileScreen} 
+                options={{
+                    title: 'Profile',
+                    headerTitleStyle: {fontWeight: 'bold', fontSize: 20},
+                    headerTintColor: globalColor.primary
+                }}
+            />
+            <Stack.Screen 
+                name="MySights" 
+                component={MySightsScreen} 
+                options={{
+                    title: 'My Sights',
+                    headerTitleStyle: {fontWeight: 'bold', fontSize: 20},
+                    headerTintColor: globalColor.primary
+                }}
+            />
         </Stack.Navigator>
     );
 }
