e52be3119952dd088b3bfa9819aac0d490aebbfe
J-Nguyen-web <map.genshin.impacts@gmail.com>
2026-03-16 20:42:27 +0200

    add dateInput for android date picker bug

---
+++ src/components/DateInput.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Platform, Text, TouchableOpacity, View } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker"
+import { globalColor } from "../globalStyles.js";
+
+export default function DateInput({ value, onChange }) {
+    const [show, setShow] = useState();
+
+    const toglePick = () => setShow(!show);
+    
+    return (
+        <View>
+            <TouchableOpacity onPress={toglePick} style={{ padding: 8, borderWidth: 1, borderRadius: 6, borderStyle: 'solid', borderColor: globalColor.turqouise}}>
+                <Text>{value ? value.toLocaleDateString() : "Select date"}</Text>
+            </TouchableOpacity>
+
+            {show && (
+                <DateTimePicker
+                    value={value || new Date()}
+                    mode="date"
+                    display={Platform.OS === "ios" ? "compact" : "default"}
+                    onChange={(event, date) => {
+                        setShow(false);
+                        if (date) onChange(date);
+                    }}
+                />
+            )}
+        </View>    
+    );
+}
