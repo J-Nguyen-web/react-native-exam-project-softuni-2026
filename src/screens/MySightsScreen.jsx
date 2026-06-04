5b1f55e1122c0a7756d7f2a9b92efc400d37810f
J-Nguyen-web <map.genshin.impacts@gmail.com>
2026-06-04 21:07:43 +0000

    add rated sights filter

--- src/screens/MySightsScreen.jsx
+++ src/screens/MySightsScreen.jsx
@@ -21,40 +21,33 @@ export default function MySightsScreen() {
     const { sights, reloadSights } = useSight();
     const { user } = useAuth();
 
-    const [ favoriteSightsId, setFavoriteSightsId ] = useState();
-    const [userRatedSightsId, setUserRatedSightsId] = useState()
 
-    const filters = {
-        created: sight => sight.ownerId === user.id,
-        favorite: sight => favoriteSightsId?.includes(sight.id),
-        rated: sight => userRatedSightsId?.includes(sight.id),
-    };
+    const [ favoriteSightsId, setFavoriteSightsId ] = useState()
 
-    const filteredSights = sights.filter(filters[type])
+    let filteredSights =
+        type === 'created'
+        ? filteredSights = sights.filter(sight => sight.ownerId === user.id)        
+        : filteredSights = sights.filter( sight => favoriteSightsId?.includes(sight.id) )
+
+    // const ratings = await getAllRatings();
+    
+    const userRated = ratings.filter( ratedSights => ratedSights.userId === user.id)
+    const userRatedSights = userRated.map( rated => rated.sightId)
+    const ratedSights = sights.filter( sight => userRatedSights.includes(sight.id))
 
     useEffect(()=> {
-        async function loadRatedSights() {
-            // const ratings = await getAllRatings();
-            // const userRated = ratings.filter( ratedSights => ratedSights.userId === user.id)
-            // const userRatedSights = userRated.map( rated => rated.sightId)
-            // const ratedSights = sights.filter( sight => userRatedSights.includes(sight.id))
-
-            // all lines in one
-            const ratedSightsId = (await getAllRatings()).filter(rates => rates.userId === user.id).map( rate => rate.sightId);
-            console.log(ratedSightsId)
-            setUserRatedSights(ratedSightsId)
-        }
-        loadRatedSights();
-
+    const ratings = await getAllRatings();
+console.log(ratings)
         async function loadUserFavorites() {
+            
             try {
                 const favoriteRef = await collection(db, 'users', user.id, 'favorites');
                 const snap = await getDocs(favoriteRef);
                 setFavoriteSightsId (snap.docs.map(doc => doc.id))
-                console.log(favoriteSightsId)
             } catch (error) {
                 console.log(error)
             }
+
         }
         loadUserFavorites();
 
